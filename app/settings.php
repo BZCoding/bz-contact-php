2cf7f1fdaef6757fc072d867e9393462e4a2b846
Vito Tardia <vito@tardia.me>
2016-08-30 16:40:12 +0200

    Fix errors on empty settings

--- app/settings.php
+++ app/settings.php
@@ -12,15 +12,15 @@ return [
         // Monolog settings
         'logger' => [
             'name' => 'bz-contact',
-            'path' => ($_SERVER['LOG_PATH']) ? $_SERVER['LOG_PATH'] : 'php://stdout',
+            'path' => (isset($_SERVER['LOG_PATH'])) ? $_SERVER['LOG_PATH'] : 'php://stdout',
         ],
 
         'database' => [
             'host' => $_SERVER['DATABASE_HOST'], // i.e mongodb://1.2.3.4:27017
             'name' => $_SERVER['DATABASE_NAME'],
             'collection' => $_SERVER['DATABASE_COLLECTION'],
-            'username' => ($_SERVER['DATABASE_USERNAME']) ? $_SERVER['DATABASE_USERNAME'] : null,
-            'password' => ($_SERVER['DATABASE_PASSWORD']) ? $_SERVER['DATABASE_PASSWORD'] : null
+            'username' => (isset($_SERVER['DATABASE_USERNAME'])) ? $_SERVER['DATABASE_USERNAME'] : null,
+            'password' => (isset($_SERVER['DATABASE_PASSWORD'])) ? $_SERVER['DATABASE_PASSWORD'] : null
         ],
 
         'mailer' => [
@@ -34,8 +34,17 @@ return [
             'thankyou_subject' => $_SERVER['MAILER_THANKYOU_SUBJECT'], // full subject
             'host' => $_SERVER['MAILER_HOST'], // Mailcatcher on Vagrant host
             'port' => $_SERVER['MAILER_PORT'],
-            'username' => ($_SERVER['MAILER_USERNAME']) ? $_SERVER['MAILER_USERNAME'] : null,
-            'password' => ($_SERVER['MAILER_PASSWORD']) ? $_SERVER['MAILER_PASSWORD'] : null
+            'username' => (isset($_SERVER['MAILER_USERNAME'])) ? $_SERVER['MAILER_USERNAME'] : null,
+            'password' => (isset($_SERVER['MAILER_PASSWORD'])) ? $_SERVER['MAILER_PASSWORD'] : null
+        ],
+
+        'amqp' => [
+            'host' => $_SERVER['AMQP_HOST'],
+            'port' => $_SERVER['AMQP_PORT'],
+            'username' => $_SERVER['AMQP_USERNAME'],
+            'password' => $_SERVER['AMQP_PASSWORD'],
+            'vhost' => $_SERVER['AMQP_VHOST'],
+            'queue' => $_SERVER['AMQP_QUEUE'],
         ],
     ],
 ];
