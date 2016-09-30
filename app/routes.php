682505fae75b9c3b217773f2f1ac1d432f87fb86
Vito Tardia <vito@tardia.me>
2016-09-30 17:17:22 +0200

    Add placeholders for Privacy/ToS pages

--- app/routes.php
+++ app/routes.php
@@ -1,16 +1,28 @@
 <?php
 // Routes
 
-$app->get('/', function ($request, $response) {
-    // Sample log message
-    $this->logger->info("GET - '/'");
+// Render ToS page
+$app->get('/terms', function ($request, $response) {
+    return $this->renderer->render($response, 'terms.phtml', [
+        'pageTitle' => 'Terms of Service'
+    ]);
+});
 
-    // Render index view
+// Render Privacy page
+$app->get('/privacy', function ($request, $response) {
+    return $this->renderer->render($response, 'privacy.phtml', [
+        'pageTitle' => 'Privacy & Cookies'
+    ]);
+});
+
+// Render main page
+$app->get('/', function ($request, $response) {
     return $this->renderer->render($response, 'index.phtml', [
         'form' => $this->form
     ]);
 });
 
+// Process form entry
 $app->post('/', function ($request, $response) {
     // Sample log message
     $this->logger->info("POST - '/'", ['ip' => $request->getAttribute('ip_address')]);
